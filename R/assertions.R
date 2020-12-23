8e30fc60b0d5260d7c01f2281217a75f4ce11d8b
Jean-Mathieu Potvin <info@jeanmathieupotvin.com>
2020-12-22 22:41:32 -0500

    Redesign function is_strict_atomic(). This version is work in progress and should flag pure vectors. See doc.
    Add better documentation of what is an Atomic value.

--- R/assertions.R
+++ R/assertions.R
@@ -3,10 +3,10 @@
 #' @title Simple assertions
 #'
 #' @description
-#' Check if an object is of a proper storage type, class and length. This
-#' is useful when implementing simple checks. These simple assertions are
-#' used extensively in \pkg{blueprint} and are available to the users for
-#' convenience.
+#' Check if an object is of a proper type, class and length. This is
+#' useful when implementing simple checks. These simple assertions are
+#' used extensively in \pkg{blueprint} and are available to the users
+#' for convenience.
 #'
 #' @param x any \R object.
 #'
@@ -15,11 +15,27 @@
 #' the function's name).
 #'
 #' @section Strict atomic values:
-#' In package \pkg{blueprint}, a *strict atomic value* is any value that has
-#' one of these following types: [NULL][base::NULL], [logical][base::logical()],
-#' [integer][base::integer()], [numeric][base::numeric()],
-#' [complex][base::complex()], [character][base::character()] or
+#' In package \pkg{blueprint}, a *strict atomic value* is any \R value
+#' (including `NULL`) that respects the following criteria.
+#'
+#' 1. It has an atomic \R type:
+#' [`NULL`][base::NULL],
+#' [logical][base::logical()],
+#' [integer][base::integer()],
+#' [single][base::single()],
+#' [double][base::double()] (or [numeric][base::numeric()])
+#' [complex][base::complex()],
+#' [character][base::character()] or
 #' [raw][base::raw()].
+#' 1. It is a pure vector that has only one non-degenerate dimension.
+#' 2. It does not possess any structural attributes (other than type-related
+#' attributes), not even [names][base::names()].
+#'
+#' Matrices, arrays and other recursive structures are **not** strict atomic
+#' values. Vectors such as [factors][base::factor()] that are described by
+#' attributes (comments, levels, etc.) are **not** strict atomic values.
+#' [Single][base::single()] values (technically, [numeric][base::numeric()]
+#' values with a `Csingle` attribute) are considered as strict atomic values.
 #'
 #' @author Jean-Mathieu Potvin (<jm@@potvin.xyz>)
 #'
@@ -60,22 +76,23 @@ is_scalar_numeric <- function(x)
 #' @export
 is_strict_atomic <- function(x)
 {
-    types <- c(
-        "NULL",
-        "logical",
-        "integer",
-        "numeric",
-        "double",
-        "complex",
-        "character",
-        "raw"
-    )
-
     if (missing(x)) {
+
+        # By design, we return FALSE when x is missing.
         return(FALSE)
-    } else if (match(class(x)[[1L]], types, 0L)) {
+    } else if (is.object(x) || is.recursive(x) || is.array(x)) {
+
+        # Fast catch of almost any S4 or S3 classes,
+        # recursive structures and atomic arrays.
+        return(FALSE)
+    } else if (is.atomic(x)) {
+
+        # Fast catch of atomic types, including NULL.
+        # TODO(JMP): catch attributes.
         return(TRUE)
     } else {
+
+        # Safety net.
         return(FALSE)
     }
 }
