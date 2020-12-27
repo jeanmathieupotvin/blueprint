7be1289dd3c3be610715baf2686601e1157aeec1
Jean-Mathieu Potvin <info@jeanmathieupotvin.com>
2020-12-27 17:16:43 -0500

    Use roxygen2 templates. Ignore them in builds.

---
+++ man-roxygen/param-validate.R
@@ -0,0 +1,4 @@
+#' @param .validate A scalar logical. Validate the object before calling
+#' the method? This argument is `TRUE` by default. While it is available
+#' to the users, it should be ignored, unless you have an alternative way
+#' to make sure your underlying object is valid.
---
+++ man-roxygen/section-self-validation.R
@@ -0,0 +1,3 @@
+#' @section Self-validation:
+#' The contents of an instance is checked when it is initialized and each time
+#' a method is called afterward.
