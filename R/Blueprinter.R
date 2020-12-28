589f4548735364c3169920a118eca9cb6fb0fcb1
Jean-Mathieu Potvin <info@jeanmathieupotvin.com>
2020-12-27 21:33:23 -0500

    Small changes to doc. Now more precise.

--- R/Blueprinter.R
+++ R/Blueprinter.R
@@ -11,18 +11,24 @@ NULL
 #' @title Concise interface to blueprint
 #'
 #' @description
-#' Create instances of \pkg{blueprint}'s classes faster and more easily.
+#' Create instances of \pkg{blueprint}'s classes with a fast and concise
+#' operator. The output's class is automatically deduced from argument
+#' `object`.
 #'
 #' @param symbol A syntactic [name], also known as an *unquoted name*.
 #'
 #' @param object Any \R value.
 #'
 #' @return The [`%bp%`][Blueprinter] operator is a convenient wrapper to
-#' \pkg{blueprint}'s constructors and returns an appropriate [R6][R6::R6]
-#' object, a [Blueprint] that corresponds to the underlying type of `object`.
+#' \pkg{blueprint}'s constructor functions and always returns an appropriate
+#' [R6][R6::R6] object. The output is always a [Blueprint] that corresponds
+#' to the underlying type of `object`.
 #'
 #' * If `object` is a [strict atomic value][is_strict_atomic()],
 #' an [Atomic] object is returned.
+#'
+#' If no [Blueprint] class corresponding to `object` is available, a
+#' `NULL` value is returned with a warning.
 #'
 #' @export
 `%bp%` <- function(symbol, object)
@@ -30,6 +36,9 @@ NULL
     if (is_strict_atomic(object)) {
         return(Atomic$new(object, deparse(substitute(symbol)), length(object)))
     } else {
+        warning("No suitable blueprint class was found for argument 'object'.",
+                call. = FALSE)
+
         return(NULL)
     }
 }
