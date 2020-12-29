debf2c139cda1926d238254c3967a60d1c6cb5ac
Jean-Mathieu Potvin <info@jeanmathieupotvin.com>
2020-12-28 19:12:10 -0500

    New objects used in unit tests of class Atomic.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,8 +2,25 @@
 
 
 # Function to suppress outputs stemming from calls to cat().
-quiet <- function(x, file = tempfile()) {
+quiet <- function(x, file = tempfile())
+{
     sink(file)
     on.exit({ sink(); file.remove(file) })
     return(invisible(x))
 }
+
+
+# Function to construct single vectors easily.
+new_single <- function(x)
+{
+    return(structure(as.double(x), Csingle = TRUE))
+}
+
+
+# Pseudo NA values for atomic types.
+NA_single_ <- new_single(NA_real_)
+NA_raw_    <- as.raw("00")
+
+
+# Function to get prototype (a private field) from class Atomic.
+proto <- function(x) { return(x$.__enclos_env__$private$prototype) }
